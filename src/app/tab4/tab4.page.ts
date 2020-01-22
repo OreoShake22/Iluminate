fa4c0672571119d0d309850f795ae91140495e32
Julen_Alzaa <julenalzaa@gmail.com>
2020-01-22 08:39:41 +0100

    perfil de usuario funcional

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthenticateService } from '../services/autentication.service';
+import {UsuarioService} from '../services/usuario.service';
+import { rankingTask } from "../models/model.interface";
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-tab4',
@@ -7,14 +10,31 @@ import { AuthenticateService } from '../services/autentication.service';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
-
+  usuario:rankingTask={
+    uid:'',
+    username:'',
+    puntuacionS:0,
+    puntuacionG:0,
+  };
+  name:string="";
+  mail:string="";
   constructor(
     private authService: AuthenticateService,
+    private usuarioservice:UsuarioService
   ) {}
 
 
   logOut(value){
     this.authService.logoutUser()
     
+  }
+  
+
+  ionViewWillEnter ()
+  {
+    this.usuarioservice.getUsuario(firebase.auth().currentUser.uid).subscribe(algo=>{
+      this.usuario=algo;
+    })
+    this.mail=firebase.auth().currentUser.email
   }
 }
