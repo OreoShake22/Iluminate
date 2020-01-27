9fe80ba0ea8975322f62fa650d56a45f219716b0
unknown <Adminpremialocal@HP-PROBOOK-14.uni.lan>
2020-01-27 12:12:09 +0100

    Orain bai egunean behin jolastu funtzionala

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {rankingservice} from '../services/ranking.service';
 import { rankingTask } from "../models/model.interface";
-import { TimeService } from '../services/time.service';
 
 @Component({
   selector: 'app-tab3',
@@ -9,20 +8,12 @@ import { TimeService } from '../services/time.service';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit{
-  dia:any;
-  constructor( public timeServices:TimeService) {
+  constructor( ) {
   }
   ngOnInit()
     {
       
     }
   
-    getPosts() { //llamamos a la funcion getPost de nuestro servicio.
-      this.timeServices.getHour()
-      .then(data => {
-        this.dia = data;
-        data=this.dia.substring(0,this.dia.indexOf("T"));
-        console.log(data)
-      });
-    }
+
 }
